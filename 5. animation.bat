e217ae4e7bfa900780f1d01542c2057cbda7cd33
rexfariz <61054639+rexfariz@users.noreply.github.com>
2020-07-29 15:29:16 +0700

    Add files via upload
    + animation.bat

---
+++ 5. animation.bat
@@ -0,0 +1,43 @@
+@echo off
+:start
+color f0
+
+echo                      'oooooooooo..oooooooooo'
+  ping localhost -n 1 -w 1.0 >nul
+echo                  'o::::::::::::::::::::::::::::o'
+  ping localhost -n 1 -w 1.0 >nul
+echo               'o::::::::::::::::::::::::::::::::::o'
+ ping localhost -n 1 -w 1.0 >nul
+echo            'o::::::::::::::::::::::::::::::::::::::::o'
+ ping localhost -n 1 -w 1.0 >nul
+echo         'o::::::::::::::::::::::::::::::::::::::::::::::o'
+ ping localhost -n 1 -w 1.0 >nul
+echo      'o::::::::::::::::::::::::::::::::::::::::::::::::::::o'
+ ping localhost -n 1 -w 1.0 >nul
+color e
+echo   'o::::::::::::::::::::::::::::::::::::::::::::::::::::::::::o'
+ ping localhost -n 1 -w 1.0 >nul
+echo      'o::::::::::::::::::::::::::::::::::::::::::::::::::::o'
+ ping localhost -n 1 -w 1.0 >nul
+echo         'o::::::::::::::::::::::::::::::::::::::::::::::o'
+ ping localhost -n 1 -w 1.0 >nul
+echo            'o::::::::::::::::::::::::::::::::::::::::o'
+ ping localhost -n 1 -w 1.0 >nul
+echo               'o::::::::::::::::::::::::::::::::::o'
+ ping localhost -n 1 -w 1.0 >nul
+echo                  'o::::::::::::::::::::::::::::o'
+ ping localhost -n 1 -w 1.0 >nul
+color 17
+echo                     'o::::::::::::::::::::::o'
+ ping localhost -n 1 -w 1.0 >nul
+echo                        'o::::::::::::::::o'
+ ping localhost -n 1 -w 1.0 >nul
+echo                           'o::::::::::o'
+ ping localhost -n 1 -w 1.0 >nul
+echo                              'oooooo'
+ ping localhost -n 1 -w 1.0 >nul
+echo                                 ..
+ ping localhost -n 1 -w 1.0 >nul
+echo.
+
+goto start
